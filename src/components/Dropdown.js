39e08d08ef876c6b415474f0f1bf6ebdabde5ba5
jessicaxu0605 <91295485+jessicaxu0605@users.noreply.github.com>
2022-12-17 20:53:30 -0700

    Update Dropdown.js

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,13 +4,17 @@ function Dropdown(props) {
     const [dropdownOpen, setdropdownOpen] = useState(false);
     const toggleDropdown = () => {dropdownOpen ? setdropdownOpen(false) : setdropdownOpen(true); console.log('toggled')};
     
+
     return(
-        <div className='dropdown-section'>
-            {dropdownOpen ? props.ExpandedContent : props.DefaultContent} 
-            <props.ToggleType Dropdown={dropdownOpen} Toggled={toggleDropdown}/>
+        <div className='dropdown-section' style={{margin:'1rem 0'}}>
+             {dropdownOpen ? props.ExpandedContent : props.DefaultContent} 
+             <div>
+                <props.ToggleType Dropdown={dropdownOpen} Toggled={toggleDropdown} {...props.ToggleAttributes}/>
+             </div>
         </div>
     );
 }
 
 export default Dropdown;
 
+//<props.ToggleType Dropdown={dropdownOpen} Toggled={toggleDropdown}/>
