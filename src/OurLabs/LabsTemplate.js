87d02fb07750f43e1d28e98c6ed6fa13cd75b5bf
jessicaxu0605 <91295485+jessicaxu0605@users.noreply.github.com>
2022-12-17 20:50:32 -0700

    Update LabsTemplate.js

--- src/OurLabs/LabsTemplate.js
+++ src/OurLabs/LabsTemplate.js
@@ -2,12 +2,13 @@ import Dropdown from "../components/Dropdown";
 import Button from "../components/Button";
 
 function LabsTemplate (props) {
+
     const defaultContent = 
         <div className={props.Classes}>
             <h2>{props.Heading}</h2>
             {props.DefaultContent}
         </div>
-    
+
     if (props.ExpandedContent==null){
         return(defaultContent);
     }
@@ -19,11 +20,25 @@ function LabsTemplate (props) {
                 {props.ExpandedContent}
             </div>
 
+    const buttonAttributes = 
+        {Text:'Show Less',
+        ClickedText:'Show More',
+        DefaultStyle: {
+            color:'white',
+            borderColor:'var(--io-blue)',
+            backgroundColor:'var(--io-blue)'
+        },
+        ActivatedStyle:{
+            boxShadow:'var(--standard-shadow'
+        }}
+
+
         return( 
             <Dropdown 
                 DefaultContent={defaultContent}
                 ExpandedContent={expandedContent}
                 ToggleType={Button} 
+                ToggleAttributes={buttonAttributes}
                 className='lab-section'/>
         );
     }
