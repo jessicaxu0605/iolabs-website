edb5c09f21316564259e3ab160339679d2c07e32
jessicaxu0605 <91295485+jessicaxu0605@users.noreply.github.com>
2022-12-17 20:51:11 -0700

    Update OurLabsPage.js

--- src/OurLabs/OurLabsPage.js
+++ src/OurLabs/OurLabsPage.js
@@ -1,14 +1,27 @@
+import React from 'react';
 import {Advocacy, Education} from './LabsSections';
+import HeaderBanner from '../components/HeaderBanner';
+import HeaderBackground from '../assets/our-labs-banner.png';
 
 function OurLabsPage() {
     return(
-        <main className='our-labs-page'>
-            <div className='main-connector'/>
-            <div className='bullet'/>
-            <Advocacy/>
-            <div className='bullet'/>
-            <Education/>
-        </main>
+        <React.Fragment>
+            <HeaderBanner
+                    ImageSrc = {HeaderBackground}
+                    Text = 'Our Labs'
+            />
+            <main className='our-labs-page'>
+                <div className='main-connector'/>
+                <section>
+                    <div className='bullet'/>
+                    <Advocacy/>
+                </section>
+                <section>
+                    <div className='bullet'/>
+                    <Education/>
+                </section>
+            </main>
+        </React.Fragment>
     );
 }
 
